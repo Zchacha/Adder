e1fcde1df909bb182daa7e153766c04179777831
Palida <6005201@kmitl.ac.th>
2018-09-10 18:47:59 +0700

    add:Adder.java

---
+++ Adder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Adder
+{
+  public static void main(String [] args)
+       {
+         Scanner sc=new Scanner(System.in);
+       System.out.print("Enter X=  ");
+        int x=sc.nextInt();
+       System.out.print("Enter Y=  ");
+        int y=sc.nextInt();
+      int z=x+y;
+       System.out.println("Ans= "+z);
+  }
+}
+      
